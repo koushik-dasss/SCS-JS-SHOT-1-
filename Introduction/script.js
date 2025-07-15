4d9e1caf9834621a163442414fa942c6ce5fd177
koushik-dasss <koushikdasmagnate2005@gmail.com>
2025-07-15 23:59:09 +0530

    Introduction and self research on variables , hoisting etc

---
+++ Introduction/script.js
@@ -0,0 +1,38 @@
+// words vs keywords 
+
+// keywords are special words that have a special meaning in a language
+
+// words are random words 
+
+// variables are used to store values in a program
+// it is made just to use the data stored in memory for reuse in the same program in the future for further execution of the program 
+
+// let var const 
+
+// const no value change 
+// let and var 
+// var old way of declaring variables in js 
+// let is the new way of declaring variables in js
+
+//  var, let, and const – Line-by-Line Comparison
+
+// var = es5 feature 
+// let , const = es6 feature
+
+// var let const 
+a = 12; // global scoped variable , not block scoped , should not be practiced in modern js
+var a ; // declared (undefined due to hoisting to top of scope of variable declared )
+var a = 12 ; // initialized 
+var a = 13; // reinitialized or can say redclared also 
+a = 13 ; // reassigned 
+let a ; // declared 
+let a = 12; // initialized 
+a = 13 ; // reassigned
+let a = 13 ; 
+// const a ;
+// const a ;  error because const can't be declared and left for later reinitialization 
+// const variables are to be declared and initialized at the same time along with no access to reassignment of values which is allowed in let and var 
+const a = 12;
+
+
+
