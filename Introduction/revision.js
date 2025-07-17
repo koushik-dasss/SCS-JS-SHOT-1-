38e834489597363378f5773dc0db1cabffb69856
koushik-dasss <koushikdasmagnate2005@gmail.com>
2025-07-17 23:59:26 +0530

    Cheat day revision

---
+++ Introduction/revision.js
@@ -0,0 +1,12 @@
+var a ;  // declared 
+a= 10 ; // initialized 
+a = 20 ; // reassigned 
+var a = 20 ; // redeclared 
+
+let b ; // declared 
+b = 10 ; // intialized 
+b = 20 ; // reassigned 
+// let b = 30 ; // error cant be redeclared , but can only be reassigned 
+
+// const a ; // error cant be declared only , need to be initialized at the same time 
+
