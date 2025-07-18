1f21849f83da7bc16b9572c2ce8c899e1f1d90ff
koushik-dasss <koushikdasmagnate2005@gmail.com>
2025-07-18 23:59:05 +0530

    rev and exp work

---
+++ Introduction/experiment.js
@@ -0,0 +1,13 @@
+let a ; 
+console.log(a);
+let b = null ; 
+console.log(b);
+console.log(typeof(a)); // undefined 
+console.log(typeof(b)); // object 
+console.log(typeof(NaN)); // number
+console.log(NaN===NaN); // false 
+console.log(typeof NaN === "number"); // true 
+
+
+
+
