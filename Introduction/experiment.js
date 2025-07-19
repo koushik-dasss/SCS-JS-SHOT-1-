6c36a76bd94efdcae4120b1ca34de0bd1f523b6b
koushik-dasss <koushikdasmagnate2005@gmail.com>
2025-07-19 23:59:47 +0530

    random commit

--- Introduction/experiment.js
+++ Introduction/experiment.js
@@ -7,6 +7,26 @@ console.log(typeof(b)); // object
 console.log(typeof(NaN)); // number
 console.log(NaN===NaN); // false 
 console.log(typeof NaN === "number"); // true 
+console.log(2==2);
+console.log(2=="2");
+console.log(2==="2");
+console.log(2!=2);
+console.log(2!="2");
+console.log(2!=="2");
+console.log(2>"2");
+console.log(2>2);
+
+
+console.log(2>3 && 3>2); //false
+console.log(2<3 && 3<2); //false
+console.log(2>3 && 3<2); //false
+console.log(2>1 && 3>2); //true
+
+console.log(2>3 || 3>2); //true
+console.log(2<3 || 3<2); //true
+console.log(2>3 || 3<2); //false
+console.log(2>1 || 3>2); //true
+
 
 
 
