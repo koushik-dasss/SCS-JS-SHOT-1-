28f58367bb89298f97395c275958d73fb5dfcc83
koushik-dasss <koushikdasmagnate2005@gmail.com>
2025-07-24 23:59:21 +0530

    loop practice qs

--- Introduction/loops_practice.js
+++ Introduction/loops_practice.js
@@ -1,44 +1,44 @@
-// for(let i = 1 ; i< 11 ; i++){
-//     console.log(i);
+for(let i = 1 ; i< 11 ; i++){
+    console.log(i);
     
-// }
+}
 
-// let i = 10;
-// while(i>0){
-//     console.log(i);
-//     i--;
-// }
+let i = 10;
+while(i>0){
+    console.log(i);
+    i--;
+}
 
-// for(let i = 1 ; i<=20 ; i++){
-//     if(i%2===0){
-//         console.log(i);
-//     }
-// }
+for(let i = 1 ; i<=20 ; i++){
+    if(i%2===0){
+        console.log(i);
+    }
+}
 
-// let j = 1 ;
-// while(j<=15){
-//     if(j%2!=0){
-//         console.log(j);
-//     }
-//     j++;
-// }
+let j = 1 ;
+while(j<=15){
+    if(j%2!=0){
+        console.log(j);
+    }
+    j++;
+}
 
-// for(let i = 1 ; i<=10 ; i++){
-//     console.log(`5*${i}=${5*i}`);
+for(let i = 1 ; i<=10 ; i++){
+    console.log(`5*${i}=${5*i}`);
     
-// }
+}
 
-// let sum = 0 ; 
-// for(let i = 1 ; i<=100 ; i++){
-//     sum+=i;
-// }
-// console.log(sum);
+let sum = 0 ; 
+for(let i = 1 ; i<=100 ; i++){
+    sum+=i;
+}
+console.log(sum);
 
-// for(let i = 1 ; i<= 50 ; i++){
-//     if(i%3==0){
-//         console.log(i);
-//     }
-// }
+for(let i = 1 ; i<= 50 ; i++){
+    if(i%3==0){
+        console.log(i);
+    }
+}
 
 // let input = parseInt(20);
 // for(let i = 1 ; i<= input ; i++){
@@ -51,21 +51,21 @@
     
 // }
 
-// let input = prompt("Enter a number : ");
-// for(let i = 1 ; i<= input ; i++){
-//     if(i%2===0){
-//         console.log(`${i} is even`);
-//     }else{
-//         console.log(`${i} is odd`);
+let input = prompt("Enter a number : ");
+for(let i = 1 ; i<= input ; i++){
+    if(i%2===0){
+        console.log(`${i} is even`);
+    }else{
+        console.log(`${i} is odd`);
         
-//     }
-// }
+    }
+}
 
 
 
-// for(let i = 1 ; i<= 100 ; i++){
-//     if(i%3==0 && i%5==0){
-//         console.log(i);
-//     }
-// }
+for(let i = 1 ; i<= 100 ; i++){
+    if(i%3==0 && i%5==0){
+        console.log(i);
+    }
+}
 
