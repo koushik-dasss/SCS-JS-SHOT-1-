a1b754c2f0d2d25737c2b1039c13f6194025aac3
koushik-dasss <koushikdasmagnate2005@gmail.com>
2025-07-25 23:59:28 +0530

    break & cont problems

--- Introduction/break&cont.js
+++ Introduction/break&cont.js
@@ -1,9 +1,9 @@
-// for(let i = 1 ; i<=100 ; i++){
-//     console.log(i);
-//      if(i%7===0){
-//         break;
-//     }
-// }
+for(let i = 1 ; i<=100 ; i++){
+    console.log(i);
+     if(i%7===0){
+        break;
+    }
+}
 
 for(let i = 1 ; i<=20 ; i++){
     if(i%3===0){
