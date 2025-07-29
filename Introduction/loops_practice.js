f6d27c2f35d6cf9888015308584a770458ae5e4c
koushik-dasss <koushikdasmagnate2005@gmail.com>
2025-07-29 23:56:34 +0530

    deleted function

--- Introduction/loops_practice.js
+++ Introduction/loops_practice.js
@@ -68,20 +68,3 @@ for(let i = 1 ; i<= 100 ; i++){
         console.log(i);
     }
 }
-
-function UglyNumber(n){
-    if(n<=0){
-        return false;
-    }
-    while(n%2==0){
-        n/=2;
-    }
-    while(n%3==0){
-        n/=3;
-    }
-    while(n%5==0){
-        n/=5;
-    }
-   return n == 1 ;
-}
-console.log(UglyNumber(17));
